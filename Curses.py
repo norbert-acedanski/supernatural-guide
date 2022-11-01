a739eb90bc608a737e171f029a56806e2727f790
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-11-01 23:46:19 +0100

    Refactor Curses.py file a bit.

--- Curses.py
+++ Curses.py
@@ -1,37 +1,38 @@
 from Colors import bcolors
 
-class Curse():
+
+class Curse:
     def __init__(self, name):
         self.name = name
         self.symptoms = None
-        self.disableMethods = None
+        self.disable_methods = None
 
-    def symptomsBase(self, symptoms = []):
+    def symptoms_base(self, symptoms=[]):
         self.symptoms = symptoms
 
-    def disableMethodsBase(self, disableMethods = []):
-        self.disableMethods = disableMethods
+    def disable_methods_base(self, disable_methods=[]):
+        self.disable_methods = disable_methods
 
-    def printName(self):
+    def print_name(self):
         print(bcolors.GREEN + "\n" + self.name + ":" + bcolors.ENDC)
     
-    def printSymptomsBase(self):
+    def print_symptoms_base(self):
         print(bcolors.YELLOW + "Clues:" + bcolors.ENDC)
-        if self.symptoms != None:
+        if self.symptoms is not None:
             for symptom in self.symptoms:
                 print("  *  " + symptom)
         else:
             print("  *  None found")
 
-    def printDisableMethods(self):
-            print(bcolors.MAGENTA + "Disable Methods:" + bcolors.ENDC)
-            if self.disableMethods != None:
-                for method in self.disableMethods:
-                    print("  *  " + method)
-            else:
-                print("  *  None found...")
+    def print_disable_methods(self):
+        print(bcolors.MAGENTA + "Disable Methods:" + bcolors.ENDC)
+        if self.disable_methods is not None:
+            for method in self.disable_methods:
+                print("  *  " + method)
+        else:
+            print("  *  None found...")
 
-    def printAll(self):
-        self.printName()
-        self.printSymptomsBase()
-        self.printDisableMethods()
+    def print_all(self):
+        self.print_name()
+        self.print_symptoms_base()
+        self.print_disable_methods()
