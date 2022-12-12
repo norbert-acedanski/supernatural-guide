806c6a0d300b84d9921b155d87b02d19ebe982fd
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-12 11:10:01 +0100

    Add PlacesBase class with Hell place along with clues from S03E16. Add methods for this class. Add PlacesClues class with a few clues for Hell.

---
+++ places_base.py
@@ -0,0 +1,24 @@
+from colors import Colors
+from place import Place
+from places_data import PlaceClues
+
+
+class PlacesBase:
+    def __init__(self):
+        self.clues = [clue for key, clue in list(PlaceClues.__dict__.items()) if not key.startswith("__")]
+
+        self.hell = Place("Hell", description="Place of eternal torture.", episodes={"S03": [16]})
+        self.hell.clues = [PlaceClues.people_chained_in_a_void, PlaceClues.electrical_storms, PlaceClues.dark_place,
+                           PlaceClues.people_chained_in_a_void]
+
+        self.places = [place for place in self.__dict__.values() if isinstance(place, Place)]
+
+    def print_places_names(self):
+        sorted_places = sorted([place.name for place in self.places])
+        print(Colors.RED + Colors.BOLD + "All places:" + Colors.ENDC)
+        for place in sorted_places:
+            print(" *  " + place)
+
+    def print_all_places(self):
+        for place in self.places:
+            place.print_all()
---
+++ places_data.py
@@ -0,0 +1,5 @@
+class PlaceClues:
+    people_chained_in_a_void = "people chained in a void"
+    electrical_storms = "electrical storms"
+    dark_place = "dark place"
+    people_screaming_in_agony = "people screaming in agony"
