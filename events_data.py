318c75d2bf62f0a768aa87f35373d9d14bdb44df
Norbert Acedanski <norbert.acedanski@gmail.com>
2022-12-26 21:51:03 +0100

    Add last clue, update description for Apocalypse based on S04E22.

--- events_data.py
+++ events_data.py
@@ -1,11 +1,12 @@
 class EventClues:
-    righteous_man_sheds_blood_in_hell = "righteous man sheds blood in Hell - the first seal"
+    first_seal = "first seal - righteous man sheds blood in Hell"
     rise_of_the_witnesses = "rise of the Witnesses (ghosts with stigma)"
     summoning_of_samhain = "summoning of Samhain"
     rippers_killed_under_the_solstice_moon = "two rippers killed under the solstice moon"
     ten_extinct_species_in_one_day = "10 extinct species in one day"
     fifteen_fishing_man_blind = "15 fishing man blind"
     somebody_killing_66_children = "somebody killing 66 children"
+    last_seal = "last seal - killing Lucifer's first (Lilith) at St. Mary's convent. Ilchester, Maryland"
 
 
 class EventDisableMethods:
