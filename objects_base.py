687f5c151f553ac2fde00b77b7bf2590142e20b6
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-02-21 22:01:22 +0100

    Add usage of Demon Killing Knife from S05E20.

--- objects_base.py
+++ objects_base.py
@@ -46,7 +46,8 @@ class ObjectsBase:
         self.demon_killing_knife = Object("Demon killing knife", description="Can literally kill demons, "
                                                                              "not get them back to hell. "
                                                                              "Cannot kill certain kinds of demons.",
-                                          episodes={"S03": [1, 9, 16], "S04": [1, 9, 20, 22], "S05": [1, 10, 14, 17]})
+                                          episodes={"S03": [1, 9, 16], "S04": [1, 9, 20, 22],
+                                                    "S05": [1, 10, 14, 17, 20]})
         self.demon_killing_knife.abilities = [ObjectAbilities.can_kill_demons, ObjectAbilities.cannot_kill_angels]
 
         self.lucky_rabbits_foot = Object("Lucky rabbits foot",
