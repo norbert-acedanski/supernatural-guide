a75fecb18fa8f9ba077d1b05b775a94e92e66427
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-03-20 23:37:33 +0100

    Add Alternate timeline encounter from S06E17.

--- places_base.py
+++ places_base.py
@@ -15,7 +15,7 @@ class PlacesBase:
         self.alternate_timeline = Place("Alternate timeline",
                                         description="A universe, where the history played out differently. "
                                                     "Can be accessed via a spell or an angel can put you in one.",
-                                        episodes={"S04": [17], "S05": [4], "S06": [15]})
+                                        episodes={"S04": [17], "S05": [4], "S06": [15, 17]})
         self.alternate_timeline.clues = [PlaceClues.different_history, PlaceClues.different_memories,
                                          PlaceClues.strange_feeling_that_things_should_be_different]
 
