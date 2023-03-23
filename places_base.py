dc467af82bb79bd5ae0312709c60fbb07a883c76
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-03-23 19:24:10 +0100

    Add Hell clue from S06E20.

--- places_base.py
+++ places_base.py
@@ -8,9 +8,9 @@ class PlacesBase:
         self.clues = [clue for key, clue in list(PlaceClues.__dict__.items()) if not key.startswith("__")]
 
         self.hell = Place("Hell", description="Place of eternal torture. People are either tortured, or torture others.",
-                          episodes={"S03": [16], "S04": [1, 8, 10]})
+                          episodes={"S03": [16], "S04": [1, 8, 10], "S06": [20]})
         self.hell.clues = [PlaceClues.people_chained_in_a_void, PlaceClues.electrical_storms, PlaceClues.dark_place,
-                           PlaceClues.flashing_lights, PlaceClues.time_runs_faster_there]
+                           PlaceClues.flashing_lights, PlaceClues.time_runs_faster_there, PlaceClues.endless_queue]
 
         self.alternate_timeline = Place("Alternate timeline",
                                         description="A universe, where the history played out differently. "
