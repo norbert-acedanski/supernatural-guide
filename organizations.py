94f4801034d789022cd6aca01fecd0a64049cbb7
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-05-12 14:25:51 +0200

    Update Man of Letters description. Add Judah Initiative and Thule Society organizations with descriptions and knowledge. S08E13

--- organizations.py
+++ organizations.py
@@ -3,11 +3,25 @@ from organizations_data import OrganizationKnowledge
 
 
 class OrganizationsBase:
-    man_of_letters = Organization("Man of letters", description="An organization, that stores knowledge "
+    man_of_letters = Organization("Man of Letters", description="An organization, that stores knowledge "
                                                                 "of the supernatural. The crest of Man of Letters is "
                                                                 "The Aquarian Star. It represents great magical power. "
                                                                 "According to Henry it stood at the gates of "
-                                                                "Atlantis itself.", episodes={"S18": [12]})
+                                                                "Atlantis itself. They have a base in Lebanon, Kansas, "
+                                                                "that contains a library of books about supernatural.",
+                                  episodes={"S18": [12, 13]})
     man_of_letters.knowledge = [OrganizationKnowledge.time_travel, OrganizationKnowledge.demon_killing_knife,
                                 OrganizationKnowledge.haitian_symbol_for_speaking_to_the_dead,
                                 OrganizationKnowledge.knights_of_hell]
+
+    the_judah_initiative = Organization("The Judah initiative", description="They were active during WW2. A group of "
+                                                                            "rabbis that were good saboteurs. "
+                                                                            "They knew Man of Letters. It was founded "
+                                                                            "to fight The Thule Society.",
+                                        episodes={"S08": [13]})
+    the_judah_initiative.knowledge = [OrganizationKnowledge.golem_magic]
+
+    the_thule_society = Organization("The Thule Society", description="Nazi necromancers. A secret fraternity, that "
+                                                                      "sponsored the early days of the Nazi Party.",
+                                     episodes={"S08": [13]})
+    the_thule_society.knowledge = [OrganizationKnowledge.immortal_life]
--- organizations_data.py
+++ organizations_data.py
@@ -3,3 +3,5 @@ class OrganizationKnowledge:
     demon_killing_knife = "Demon-killing knife of the Kurds"
     haitian_symbol_for_speaking_to_the_dead = "haitian symbol for speaking to the dead"
     knights_of_hell = "Knights of Hell"
+    golem_magic = "golem magic"
+    immortal_life = "immortal life"
