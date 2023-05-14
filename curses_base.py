ecbfeeec2aaaa5070bc36c834ee74cf60e89ff2f
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-05-14 23:12:29 +0200

    Update Closing the Gates of Hell spell based on S08E17.

--- curses_base.py
+++ curses_base.py
@@ -202,8 +202,8 @@ class CursesBase:
                                                                  "Other 2 unknown for now. After the trials there is "
                                                                  "a spell to say and the Gates of Hell will close. "
                                                                  "Spell hurts the person, that tries it - starts to "
-                                                                 "cough blood.",
-                                                     episodes={"S08": [14, 15, 16]})
+                                                                 "cough blood. Even angel healing cannot heal it.",
+                                                     episodes={"S08": [14, 15, 16, 17]})
         self.closing_the_gates_of_hell_spell.clues = [CursesClues.killed_hell_hound, CursesClues.blood_cough]
 
         self.bringing_demon_back_to_the_body_spell = Curse("Bringing demon back to the body spell",
