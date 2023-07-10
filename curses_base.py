818efc5059a2af18ee1aea460f678504bea8ba79
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-07-10 18:07:09 +0200

    Update Mark of Cain description based on S10E17.

--- curses_base.py
+++ curses_base.py
@@ -271,8 +271,9 @@ class CursesBase:
                                                      "counteract telekinetic abilities of a Knights of Hell. That "
                                                      "person can also telekinetically bring the blade to himself. If a "
                                                      "person with the Mark of Cain is killed - he becomes a demon "
-                                                     "(according to the ending of S09E23).",
-                         episodes={"S09": [11, 12, 16, 17, 18, 21], "S10": [1, 2, 3, 9, 10, 11, 12, 13]})
+                                                     "(according to the ending of S09E23). According to Metatron it is "
+                                                     "God-level (or Lucyfer-level) magic.",
+                         episodes={"S09": [11, 12, 16, 17, 18, 21], "S10": [1, 2, 3, 9, 10, 11, 12, 13, 17]})
     mark_of_cain.clues = [CursesClues.people_acting_weirdly, CursesClues.less_affected_by_telekinesis_of_knight_of_hell,
                           CursesClues.high_strength, CursesClues.black_eyes, CursesClues.increased_regeneration,
                           CursesClues.weird_dreams]
