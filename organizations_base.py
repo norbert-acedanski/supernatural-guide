f1e8ca6b2f6c3bd7567d2ed9d0323186f988d07c
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-07-10 18:06:44 +0200

    Update Man of Letters description based on S10E17.

--- organizations_base.py
+++ organizations_base.py
@@ -12,9 +12,10 @@ class OrganizationsBase:
                                                                 "that contains a library of books about supernatural. "
                                                                 "One of the members was L. Franck Baum, the writer. "
                                                                 "He wrote books about his daughter in Ozz. Charlie "
-                                                                "broke the fourth wall in their compound in S09E04.",
+                                                                "broke the fourth wall in their compound in S09E04. "
+                                                                "They have more power than the Coven of Witches.",
                                   episodes={"S08": [12, 13, 14, 16, 20, 22, 23],
-                                            "S09": [4, 5, 6, 7, 9, 11, 14, 16, 17], "S10": [3, 8, 11, 12]})
+                                            "S09": [4, 5, 6, 7, 9, 11, 14, 16, 17], "S10": [3, 8, 11, 12, 17]})
     man_of_letters.knowledge = [OrganizationKnowledge.time_travel, OrganizationKnowledge.demon_killing_knife,
                                 OrganizationKnowledge.haitian_symbol_for_speaking_to_the_dead,
                                 OrganizationKnowledge.knights_of_hell, OrganizationKnowledge.drakopoolos_journal,
