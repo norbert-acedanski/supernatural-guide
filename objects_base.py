02cfe4ba6c78041d047320f2a3a25f63a6a09ce6
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-10-07 14:38:22 +0200

    Add Angel Blade usage. Add Archangel Blade, Word of GOD, Enochian Handcuffs appearances. S13E13

--- objects_base.py
+++ objects_base.py
@@ -111,7 +111,7 @@ class ObjectsBase:
                                    "S08": [7, 10, 17, 21, 22, 23], "S09": [1, 2, 3, 6, 9, 10, 11, 14, 16, 18, 21, 22, 23],
                                    "S10": [1, 3, 7, 9, 10, 13, 17, 18, 20, 21, 22],
                                    "S11": [1, 2, 3, 6, 9, 10, 11, 15, 18, 22, 23],
-                                   "S12": [1, 3, 6, 7, 9, 10, 12, 13, 15, 17, 19, 21, 23], "S13": [1, 7, 8, 9, 10]})
+                                   "S12": [1, 3, 6, 7, 9, 10, 12, 13, 15, 17, 19, 21, 23], "S13": [1, 7, 8, 9, 10, 13]})
     angel_blade.abilities = [ObjectAbilities.can_kill_angels, ObjectAbilities.can_kill_demons,
                              ObjectAbilities.can_kill_reapers, ObjectAbilities.cannot_kill_knights_of_hell,
                              ObjectAbilities.cannot_kill_archangels]
@@ -159,7 +159,7 @@ class ObjectsBase:
     ring_of_famine.abilities = [ObjectAbilities.can_give_incredible_starving_sensation_for_something]
 
     archangel_blade = Object("Archangel blade", description="A triangular, silvery blade, that each archangel has.",
-                             episodes={"S05": [19], "S13": [7]})
+                             episodes={"S05": [19], "S13": [7, 13]})
     archangel_blade.abilities = [ObjectAbilities.can_kill_archangels]
 
     ring_of_pestilence = Object("Ring of Pestilence", description="Can give people diseases of any kind. One of the "
@@ -237,7 +237,7 @@ class ObjectsBase:
                                                     "by Lucyfer) has information about Angels and how to make "
                                                     "them fall. Found in Lucyfer crypt in S08E17.",
                          episodes={"S07": [21], "S08": [1, 2, 10, 17, 21, 23], "S09": [9, 10, 23], "S10": [18],
-                                   "S11": [6], "S13": [7]})
+                                   "S11": [6], "S13": [7, 13]})
     word_of_god.abilities = [ObjectAbilities.can_cause_storms, ObjectAbilities.can_cause_women_to_go_to_labour,
                              ObjectAbilities.when_opened_causes_a_person_to_become_a_prophet]
 
@@ -400,7 +400,7 @@ class ObjectsBase:
 
     enochian_handcuffs = Object("Enochian handcuffs", description="Handcuffs, that can limit the power of celestial "
                                                                   "beings. It seems, that Archangels are immune to it.",
-                                episodes={"S12": [7]})  # TODO: Check in which episode earlier Castiel is held in them
+                                episodes={"S12": [7], "S13": [13]})  # TODO: Check in which episode earlier Castiel is held in them
     enochian_handcuffs.abilities = [ObjectAbilities.traps_an_angel]
 
     hyperbolic_pulse_generator = Object("Hyperbolic Pulse Generator",
