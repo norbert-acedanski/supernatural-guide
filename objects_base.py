9ad00f22914413853b903b54a856a35455e0d771
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-10-15 22:24:11 +0200

    Add Angel Blade usage from S13E18.

--- objects_base.py
+++ objects_base.py
@@ -112,7 +112,7 @@ class ObjectsBase:
                                    "S10": [1, 3, 7, 9, 10, 13, 17, 18, 20, 21, 22],
                                    "S11": [1, 2, 3, 6, 9, 10, 11, 15, 18, 22, 23],
                                    "S12": [1, 3, 6, 7, 9, 10, 12, 13, 15, 17, 19, 21, 23],
-                                   "S13": [1, 7, 8, 9, 10, 13, 14]})
+                                   "S13": [1, 7, 8, 9, 10, 13, 14, 18]})
     angel_blade.abilities = [ObjectAbilities.can_kill_angels, ObjectAbilities.can_kill_demons,
                              ObjectAbilities.can_kill_reapers, ObjectAbilities.cannot_kill_knights_of_hell,
                              ObjectAbilities.cannot_kill_archangels]
