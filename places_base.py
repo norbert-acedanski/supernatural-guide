bd7a990fec9b5e492ce0c4fa87891dd493d51b2e
Norbert Acedanski <norbert.acedanski@gmail.com>
2023-11-02 11:27:00 +0100

    Add Heaven appearance from S14E08.

--- places_base.py
+++ places_base.py
@@ -32,7 +32,7 @@ class PlacesBase:
                                          "in the place, they are in and following it. Heaven is powered by Angels "
                                          "(according to Naomi from S1319).",
                    episodes={"S05": [16], "S08": [7, 10, 17, 23], "S09": [18, 22, 23], "S10": [2, 17], "S11": [18, 22],
-                             "S13": [13, 18, 19]})
+                             "S13": [13, 18, 19], "S14": [8]})
     heaven.clues = [PlaceClues.good_memories_relived, PlaceClues.changing_scenery, PlaceClues.bright_place]
 
     purgatory = Place("Purgatory", description="A place, where all monsters go, after they die. Can be opened with an "
